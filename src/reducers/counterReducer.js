0960cc5eab70fccc2e74c8f83c4a949686a676ea
zdurr22 <zdurr22@gmail.com>
2021-11-09 19:32:02 -0500

    commit tree 6f526cb0ad3bbedcb958abc695940abdfbd67151

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -4,12 +4,16 @@ export default function counterReducer(
 	},
 	action
 ) {
+	console.log(action);
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log("Current state.clicks %s", state.clicks);
+      		console.log("Updating state.clicks to %s", state.clicks + 1);
 			return {
 				clicks: state.clicks + 1
 			}
 		default:
+			console.log("Initial state.clicks: %s", state.clicks);
 			return state;
 	}
 }
